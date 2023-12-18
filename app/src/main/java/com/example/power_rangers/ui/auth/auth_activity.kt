2b05c16323dd8b6997fa38aa1ac2e55af4bb54c1
Om Nichal <omnichal888@gmail.com>
2023-12-18 16:14:01 +0530

    Signup & Login page added

--- app/src/main/java/com/example/power_rangers/ui/auth/auth_activity.kt
+++ app/src/main/java/com/example/power_rangers/ui/auth/auth_activity.kt
@@ -1,12 +1,24 @@
 package com.example.power_rangers.ui.auth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.power_rangers.R
+
 
 class auth_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun opensignup(view: View) {
+        val intent = Intent(this, signup::class.java)
+        startActivity(intent)
+    }
+    fun openlogin(view: View) {
+        val intent = Intent(this, login::class.java)
+        startActivity(intent)
+    }
+
 }
---
+++ app/src/main/java/com/example/power_rangers/ui/auth/login.kt
@@ -0,0 +1,15 @@
+package com.example.power_rangers.ui.auth
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.power_rangers.R
+
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
