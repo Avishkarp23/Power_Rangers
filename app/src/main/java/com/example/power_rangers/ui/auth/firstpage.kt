b8302b28beb35800e270edc58d67667ae2284962
Soumya Singh <soumyasingh1400304@gmail.com>
2023-12-18 20:09:21 +0530

    Splash

---
+++ app/src/main/java/com/example/power_rangers/ui/auth/firstpage.kt
@@ -0,0 +1,12 @@
+package com.example.power_rangers.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.power_rangers.R
+
+class firstpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.firstpage)
+    }
+}
