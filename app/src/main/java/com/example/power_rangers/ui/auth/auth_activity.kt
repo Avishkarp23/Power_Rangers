f00b5a92240231b462620a39159a77031f702ee4
avishkarp23 <avishkarp_t20031@students.isquareit.edu.in>
2023-12-18 12:48:47 +0530

    Restructured the app in MVVM form

--- app/src/main/java/com/example/power_rangers/ui/auth/auth_activity.kt
+++ app/src/main/java/com/example/power_rangers/ui/auth/auth_activity.kt
@@ -1,9 +1,10 @@
-package com.example.power_rangers
+package com.example.power_rangers.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.power_rangers.R
 
-class MainActivity : AppCompatActivity() {
+class auth_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
