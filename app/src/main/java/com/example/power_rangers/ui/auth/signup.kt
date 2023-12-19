0250bd95033f39e9c7b6ca20bc5ad81b3117e872
Om Nichal <omnichal888@gmail.com>
2023-12-19 10:44:26 +0530

    autocomplete

--- app/src/main/java/com/example/power_rangers/ui/auth/signup.kt
+++ app/src/main/java/com/example/power_rangers/ui/auth/signup.kt
@@ -2,11 +2,33 @@ package com.example.power_rangers.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import com.example.power_rangers.R
 
 class signup : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup)
+
+        val items = listOf("Officer","Contractor")
+
+        val autoComplete : AutoCompleteTextView = findViewById(R.id.auto_complete)
+
+        val adapter = ArrayAdapter(this,R.layout.list_view,items)
+
+        autoComplete.setAdapter(adapter)
+
+        autoComplete.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
+            val itemSelected = adapterView.getItemAtPosition(i)
+            Toast.makeText(this,"Item = $itemSelected",Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+
+
 }
