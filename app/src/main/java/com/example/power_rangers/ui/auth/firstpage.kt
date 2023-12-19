339f1be48ff455527c4c9f2d31c43e688729bd8f
Soumya Singh <soumyasingh1400304@gmail.com>
2023-12-19 10:32:02 +0530

    spash2

--- app/src/main/java/com/example/power_rangers/ui/auth/firstpage.kt
+++ app/src/main/java/com/example/power_rangers/ui/auth/firstpage.kt
@@ -1,12 +1,25 @@
 package com.example.power_rangers.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.example.power_rangers.R
 
 class firstpage : AppCompatActivity() {
+
+    // Delay time for the splash screen in milliseconds
+    private val SPLASH_DELAY: Long = 4000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.firstpage)
+
+        // Using a Handler to delay the transition to the authentication screen
+        Handler().postDelayed({
+            // Start the authentication screen
+            startActivity(Intent(this@firstpage, auth_activity::class.java))
+            finish()
+        }, SPLASH_DELAY)
     }
 }
